0a813841e513a4d478c84f3c50fd83a1d6bc4348
kalburgimanjunath <manjunathkalburgi65512@gmail.com>
2021-10-08 15:57:00 +0530

    3 files modified, 4 added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import * as React from 'react';
 import './style.css';
 import Resume from './pages/Resume';
 import Cover from './pages/Cover';
@@ -6,26 +6,51 @@ import Form from './pages/Form';
 import Welcome from './pages/Welcome';
 import MenuBar from './components/MenuBar';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-export default function App() {
-  return (
-    <div>
-      <Router>
-        <MenuBar />
-        <Switch>
-          <Route path="/cover">
-            <Cover />
-          </Route>
-          <Route path="/resume">
-            <Resume />
-          </Route>
-          <Route path="/form">
-            <Form />
-          </Route>
-          <Route path="/" exact>
-            <Welcome />
-          </Route>
-        </Switch>
-      </Router>
-    </div>
-  );
+import { themes } from './themes';
+import { ThemeProvider } from '@callstack/react-theme-provider';
+
+import ThemeChanger from './ThemeChanger';
+
+class App extends React.Component {
+  state = {
+    theme: themes.normal,
+  };
+  handleThemeChange = (themeName) => {
+    this.setState({ theme: themes[themeName] });
+  };
+  render() {
+    return (
+      <div>
+        <ThemeProvider theme={this.state.theme}>
+          <div>
+            <Router>
+              <ThemeChanger
+                onChangeTheme={this.handleThemeChange}
+                themes={Object.keys(themes)}
+              />
+              <MenuBar
+                onChangeTheme={this.handleThemeChange}
+                themes={Object.keys(themes)}
+              />
+              <Switch>
+                <Route path="/cover">
+                  <Cover />
+                </Route>
+                <Route path="/resume">
+                  <Resume />
+                </Route>
+                <Route path="/form">
+                  <Form />
+                </Route>
+                <Route path="/" exact>
+                  <Welcome />
+                </Route>
+              </Switch>
+            </Router>
+          </div>
+        </ThemeProvider>
+      </div>
+    );
+  }
 }
+export default App;
--- src/components/MenuBar.js
+++ src/components/MenuBar.js
@@ -1,33 +1,48 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
-export default function MenuBar() {
-  return (
-    <div className="navbar navbar-inverse navbar-static-top">
-      <div className="container">
-        <a href="#" className="navbar-brand"></a>{' '}
-        <button
-          className="navbar-toggle"
-          data-toggle="collapse"
-          data-target=".navHeaderCollapse"
-        ></button>
-        <div className="collapse navbar-collapse navHeaderCollapse">
-          <ul className="nav navbar-nav navbar-right">
-            <li className="active">
-              <Link to="/">Home</Link>
-            </li>
+import styled from 'styled-components';
+import { withTheme } from '@callstack/react-theme-provider';
 
-            <li>
-              <Link to="resume">Resume</Link>
-            </li>
-            <li>
-              <Link to="cover">Cover</Link>
-            </li>
-            <li>
-              <Link to="form">Fill & Export</Link>
-            </li>
-          </ul>
+function MenuBar({ theme, themes, onChangeTheme }) {
+  return (
+    <Container textColor={theme.textColor} background={theme.backgroundColor}>
+      <div className="navbar navbar-static-top">
+        <div className="container">
+          <a href="#" className="navbar-brand"></a>{' '}
+          <button
+            className="navbar-toggle"
+            data-toggle="collapse"
+            data-target=".navHeaderCollapse"
+          ></button>
+          <div className="collapse navbar-collapse navHeaderCollapse">
+            <ul className="nav navbar-nav navbar-right">
+              <li className="active">
+                <Link to="/">Home</Link>
+              </li>
+
+              <li>
+                <Link to="resume">Resume</Link>
+              </li>
+              <li>
+                <Link to="cover">Cover</Link>
+              </li>
+              <li>
+                <Link to="form">Fill & Export</Link>
+              </li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
+    </Container>
   );
 }
+const Container = styled.div`
+  box-sizing: border-box;
+  width: 100%;
+  color: ${(props) => props.textColor};
+  background-color: ${(props) => props.background};
+  text-align: center;
+  font-size: 1.4rem;
+`;
+
+export default withTheme(MenuBar);
